8f73a9c163ecf45459277fb749a59d75a70e08ee
Maxime <91160151+MaximePage@users.noreply.github.com>
2026-01-05 13:09:44 -0500

    smartwatch typescript configuration

--- src/TOMI/services/ble/bleProtocol.ts
+++ src/TOMI/services/ble/bleProtocol.ts
@@ -1,16 +1,3 @@
 import base64 from "react-native-base64";
 
-export const decodeColor = (value: string): string => {
-  const code = base64.decode(value);
-
-  switch (code) {
-    case "B":
-      return "blue";
-    case "R":
-      return "red";
-    case "G":
-      return "green";
-    default:
-      return "white";
-  }
-};
+export const decodeRaw = (value: string) => value;
